bd37ae30b3a7a8b5bb244f36f328c1676a067d13
popolala <tan.wei.jie@hotmail.com>
2020-05-06 22:27:37 +0800

    ss

--- src/main/java/Others/CheckNullObject.java
+++ src/main/java/Others/CheckNullObject.java
@@ -6,22 +6,35 @@ public class CheckNullObject {
 
     public static void main(String[] args) {
 
+        //working
         Set<String> set = new HashSet<>();
         set.add(null);
+        System.out.println("Set is "+set);
 
-        System.out.println(set);
-
+        //working
         List<String> list = new ArrayList<>();
         list.add(null);
+        System.out.println("List is "+list);
 
-        System.out.println(list);
+        //working
+        Map hashMap = new HashMap();
+        hashMap.put(null , 1);
+        System.out.println("HashMap is "+hashMap);
 
+        //working
+        List<String> linkedList = new LinkedList<>();
+        linkedList.add(null);
+        System.out.println("LinkedList is "+linkedList);
+
+        //no working
         Deque<String> deque = new ArrayDeque<String>(list); //Line n2
         deque.add(null);
+        System.out.println("Deque is "+deque);
 
-        System.out.println(deque);
-
-
+        //no working
+        Set<String> treeSet = new TreeSet<>();
+        treeSet.add(null);
+        System.out.println("TreeSet is "+set);
 
     }
 }
